d566287efbb9569cf7102e71b75deea5b5915d14
nathan <romeronathan@hotmail.com>
2021-10-24 15:59:41 -0400

    Created and worked on meal class

---
+++ src/Models/Meal.java
@@ -0,0 +1,52 @@
+package Models;
+
+public class Meal {
+
+    private int id;
+    private String name;
+    private int calories;
+    private int portion;
+
+    public Meal(String name, int calories, int portion) {
+        this.name = name;
+        this.calories = calories;
+        this.portion = portion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getPortion() {
+        return portion;
+    }
+
+    public void setPortion(int portion) {
+        this.portion = portion;
+    }
+
+
+    public String mealDescription() {
+        return this.name + " has " + this.calories + " for a portion of " + this.portion;
+    }
+}
