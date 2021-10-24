f02a1cda66f7c6f83bdd470cf6c41cfc48d57862
nathan <romeronathan@hotmail.com>
2021-10-24 15:50:18 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 400, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
