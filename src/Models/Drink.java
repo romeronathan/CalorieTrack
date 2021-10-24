78cd54777a9dd8ed18110ec89de35b6350dc5298
nathan <romeronathan@hotmail.com>
2021-10-24 16:01:35 -0400

    Created and worked on drink class

---
+++ src/Models/Drink.java
@@ -0,0 +1,52 @@
+package Models;
+
+public class Drink {
+
+    private int id;
+    private String name;
+    private int calories;
+    private int serving;
+
+    public Drink(String name, int calories, int serving) {
+        this.name = name;
+        this.calories = calories;
+        this.serving = serving;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getPortion() {
+        return serving;
+    }
+
+    public void setPortion(int portion) {
+        this.serving = portion;
+    }
+
+
+    public String drinkDescription() {
+        return this.name + " has " + this.calories + " for a portion of " + this.serving;
+    }
+}
