42db0e6a75a3884c62cf947fd7b1181a0e2f5d34
nathan <romeronathan@hotmail.com>
2021-10-31 17:24:30 -0400

    Started development on database

---
+++ src/Database/Const.java
@@ -0,0 +1,7 @@
+package Database;
+
+public class Const {
+    public static final String DB_NAME = "database.db";
+    public static final String DB_USER = "nromero";
+    public static final String DB_PASS = "spkqzspkqzcmm3vcmm3v";
+}
